7f361230255319e4404e3066155e1a2246b8248c
Daniel Barrot <byarrley@gmail.com>
2026-08-27 20:19:54 -0400

    add requirements

--- Ch01/challenge/main.go
+++ Ch01/challenge/main.go
@@ -16,6 +16,21 @@ Instructions:
   - Default to stdout
 */
 func main() {
+
+	/* Flags
+	   -width (bannerWidth): Banner width, 0 < width < 250 (default: 80)
+	    * Ensure -width is an int
+	    * Ensure that it is between 0 and 250
+	   -out (outFile): path to output file (default: stdout)
+	    * Ensure that the file can be opened
+	*/
+
+	/* Args
+		   inFile: path to input file (default: stdin)
+		     * Ensure that the file path is valid
+	       * File is non-empty text file
+	*/
+
 	text := "Go"
 	width := 6
 	Banner(os.Stdout, text, width)
