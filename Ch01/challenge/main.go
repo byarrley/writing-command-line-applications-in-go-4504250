79003b4ef9c7fe9ef139997a3fe624d2329d89d5
Daniel Barrot <byarrley@gmail.com>
2026-08-28 20:23:40 -0400

    rm shorthand, fix types in main.go

--- Ch01/challenge/main.go
+++ Ch01/challenge/main.go
@@ -10,8 +10,8 @@ import (
 /*
 Instructions:
 
-- Pass text to print as an argument
-  - If not argument - read from stdin
+- Pass text to print as an flagument
+  - If not flagument - read from stdin
 
 - Use -width to specify width
   - Width should be bigger than 0 and less than 250
@@ -33,19 +33,16 @@ func main() {
 
 	/* Args
 	   text: text to print (default: stdin)
-	     * This argument doesn't require file ops
+	     * This flagument doesn't require file ops
 			 * Unclear from video/comments what to do with multiline stdin, so print a banner for each line
-			 * For multiple args, assume that each is a separate input with its own banner (consistent with stdin treatment)
+			 * For multiple flags, assume that each is a separate input with its own banner (consistent with stdin treatment)
 	*/
 
-	var out argOut
-	var banner argWidth
+	var out flagOut
+	var banner flagWidth
 
 	flag.Var(&out, "out", "Path to output file (default: stdout)")
-	flag.Var(&out, "o", "Path to output file (default: stdout)")
-
 	flag.Var(&banner, "width", "Banner width, 0 < width < 250 (default: 80)")
-	flag.Var(&banner, "w", "Banner width, 0 < width < 250 (default: 80)")
 	flag.Parse()
 
 	text := readInput()
