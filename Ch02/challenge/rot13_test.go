d1e3b39eac870e8eb1bd7158fb634e59d09f589d
Daniel Barrot <byarrley@gmail.com>
2026-08-30 15:31:03 -0400

    add exe test

--- Ch02/challenge/rot13_test.go
+++ Ch02/challenge/rot13_test.go
@@ -1,6 +1,13 @@
 package main
 
-import "testing"
+import (
+	"os/exec"
+	"path"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 /*
 Write tests that will execute rot13.
@@ -18,7 +25,7 @@ type testCase struct {
 func TestRot13Func(t *testing.T) {
 	tests := []testCase{
 		{"initial", "test", "grfg"},
-		{"sub1", "Usenet", "Hfrarg"},
+		{"sub1", "USENET", "HFRARG"},
 		{"sub2", "equivalent", "rdhvinyrag"},
 		{"sub3", "printing", "cevagvat"},
 	}
@@ -32,4 +39,35 @@ func TestRot13Func(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestRot13Exe(t *testing.T) {
+	tests := []testCase{
+		{"sub1", "test", "grfg"},
+		{"sub2", "USENET", "HFRARG"},
+		{"sub3", "equivalent", "rdhvinyrag"},
+		{"sub4", "printing", "cevagvat"},
+	}
+
+	exe := buildExe(t)
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			got, err := exec.Command(exe, test.in).CombinedOutput()
+
+			require.NoError(t, err, "run:\n%s", string(got))
+
+			if strings.TrimSpace(string(got)) != test.want {
+				t.Fatalf("in=%s, want=%s, got=%s", test.in, test.want, got)
+			}
+		})
+	}
+
+}
+
+func buildExe(t *testing.T) string {
+	exe := path.Join(t.TempDir(), "rot13")
+	output, err := exec.Command("go", "build", "-o", exe).CombinedOutput()
+	require.NoErrorf(t, err, "build:\n%s", string(output))
+
+	return exe
 }
