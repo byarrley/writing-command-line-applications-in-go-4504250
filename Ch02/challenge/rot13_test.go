869d46f2b92082adff63b1e9ce64aaf95e5238ef
Daniel Barrot <byarrley@gmail.com>
2026-08-30 15:11:05 -0400

    add initial function test

--- Ch02/challenge/rot13_test.go
+++ Ch02/challenge/rot13_test.go
@@ -1,8 +1,26 @@
 package main
 
+import "testing"
+
 /*
 Write tests that will execute rot13.
 - Test with text as argument
-	- Test with various inputs (table test, see t.Run)
+  - Test with various inputs (table test, see t.Run)
+
 - Test with text from standard input
 */
+type testCase struct {
+	name string
+	in   string
+	want string
+}
+
+func TestRot13Func(t *testing.T) {
+	test := testCase{"initial", "test", "grfg"}
+
+	got := rot13(test.in)
+
+	if got != test.want {
+		t.Fatalf("in=%s, want=%s, got=%s", test.in, test.want, got)
+	}
+}
