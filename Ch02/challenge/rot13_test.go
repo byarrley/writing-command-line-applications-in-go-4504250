547165f5c68707ae8194f860a0fda08060b9fea3
Daniel Barrot <byarrley@gmail.com>
2026-08-30 16:15:00 -0400

    add stdin test

--- Ch02/challenge/rot13_test.go
+++ Ch02/challenge/rot13_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"io"
 	"os/exec"
 	"path"
 	"strings"
@@ -54,6 +55,36 @@ func TestRot13Exe(t *testing.T) {
 		t.Run(test.name, func(t *testing.T) {
 			got, err := exec.Command(exe, test.in).CombinedOutput()
 
+			require.NoError(t, err, "run:\n%s", string(got))
+
+			if strings.TrimSpace(string(got)) != test.want {
+				t.Errorf("in=%s, want=%s, got=%s", test.in, test.want, got)
+			}
+		})
+	}
+}
+
+func TestRot13Stdin(t *testing.T) {
+	//Adapted from example at https://pkg.go.dev/os/exec#example-Cmd.StdinPipe
+	tests := []testCase{
+		{"simple", "test", "grfg"},
+		{"all_caps", "USENET", "HFRARG"},
+		{"special_chars", "</>", "</>"},
+		{"numerals", "12345", "12345"},
+	}
+
+	exe := buildExe(t)
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			cmd := exec.Command(exe)      //set up cmd
+			stdin, err := cmd.StdinPipe() //prep stdin
+
+			go func() {
+				defer stdin.Close()
+				io.WriteString(stdin, test.in) //write input to stdin in goroutine
+			}()
+
+			got, err := cmd.CombinedOutput() //run the command, output is merged stdout/stderr (2>&1)
 			require.NoError(t, err, "run:\n%s", string(got))
 
 			if strings.TrimSpace(string(got)) != test.want {
